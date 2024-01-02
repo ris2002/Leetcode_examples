fcc43023de4319b157d06114a9310f6dcc52e7bb
Sri <sri17.boddula@gmail.com>
2024-01-03 02:49:55 +0530

    UserInput program

--- src/com/step1/step11/UserInputOutput.java
+++ src/com/step1/step11/UserInputOutput.java
@@ -1,4 +1,24 @@
 package com.step1.step11;
 
+/*
+https://takeuforward.org/java/basic-input-and-output-in-java/
+
+Input: 4
+Output:
+Value of x: 4
+
+Process finished with exit code 0
+ */
+
+import java.util.Scanner;
+
 public class UserInputOutput {
+
+    public static void main(String[] args) {
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        System.out.println("Value of x: " + x);
+        scanner.close();
+    }
 }
